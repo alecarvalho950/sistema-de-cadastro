a5b38cf7ec326cb6cb96940fa0c7d91cc6c247fc
alecarvalho950 <107328643+alecarvalho950@users.noreply.github.com>
2023-01-19 22:12:35 -0300

    corrigindo o app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+//corrigindo o app.js
 console.log("O Paulo fez esse commit sem você ver na aula")
